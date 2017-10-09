ba9ec7a5572472f5a9147867c572fd17d67a980d
eliasjawed <contact@elijawed.com>
2017-10-09 01:06:39 -0700

    Finished homework except extra credit

--- src/prototype.js
+++ src/prototype.js
@@ -50,6 +50,45 @@
 */
 
 /* eslint-disable no-undef */
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+}
+
+GameObject.prototype.destroy = function destroy() {
+  return 'Game object was removed from the game.';
+};
+
+class NPC {
+  constructor(options) {
+    GameObject.call(this, options);
+    this.hp = options.hp;
+    this.name = options.name;
+  }
+}
+
+NPC.prototype = Object.create(GameObject.prototype);
+
+NPC.prototype.takeDamage = function takeDamage() {
+  return `${this.name} took damage.`;
+};
+
+class Humanoid {
+  constructor(options) {
+    NPC.call(this, options);
+    this.faction = options.faction;
+    this.weapons = options.weapons;
+    this.language = options.language;
+  }
+}
+
+Humanoid.prototype = Object.create(NPC.prototype);
+
+Humanoid.prototype.greet = function greet() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
 
 module.exports = {
   GameObject,
